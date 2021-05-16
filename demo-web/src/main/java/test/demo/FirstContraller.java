61ad65c5570db2f3f4d7c90f2311b2f89b31b4ff
BENZJ <jbcbenz@gmail.com>
2021-05-16 16:19:33 +0800

    修改目录结构

---
+++ demo-web/src/main/java/test/demo/FirstContraller.java
@@ -0,0 +1,17 @@
+package test.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.var;
+
+@Controller
+public class FirstContraller {
+    // @RequestMapping(value = "/getDateAndTime")
+    // public ApiResponse getDateAndTime() {
+
+    // }
+}
